4b1539d02dd5cdc5ae7d7095b80202e126928a59
AsinChen <shinningstar1001@gmail.com>
2016-09-23 13:48:22 +0800

    add iisnode support

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var fetch = require('node-fetch');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
 
 
 var app = express();
 app.use(express.static(__dirname));
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 app.get('/api/getNetentSessionId', function (req, res) {
-    var data = { "membercode": req.query.membercode, "channel": req.query.channel }
+    var data = { "membercode": req.query.membercode, "channel": req.query.channel };
     var content = JSON.stringify(data);
             console.log(content);
     fetch('http://netent.188bet.qat/NetEntService.svc/loginuser',
@@ -23,9 +23,9 @@ app.get('/api/getNetentSessionId', function (req, res) {
         }).then(function (text) {
             console.log(text);
             res.send(text);
-        })
+        });
 });
 
-app.listen(3333, function () {
+app.listen(process.env.PORT || 3333, function () {
     console.log('app listen on 3333');
-})
+});
